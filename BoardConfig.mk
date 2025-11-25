8408ad1a77c1803452dfbd8582d88570912743b8
marinoalfonse748-cyber <marinoalfonse748@gmail.com>
2025-11-25 16:33:17 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,8 +97,7 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/waipio_GKI.config \
     vendor/marble_GKI.config \
-    vendor/debugfs.config \
-    vendor/extra.config
+    vendor/debugfs.config
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
